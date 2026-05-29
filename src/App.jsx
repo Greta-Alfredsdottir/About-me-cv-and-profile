da3a5f8ec4f725b5e7b71c28667d941e32991d66
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-05-29 14:41:25 +0000

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/navbar'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -7,21 +7,20 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <>
+    <section>
       <Navbar />
 
       <main>
+        <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/concepts' element={<Concepts />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
 
-          {/* NoPage Route */}
-          <Route path='*' element={<NoPage />} />
         </Routes>
+        </BrowserRouter>
       </main>
-    </>
+    </section>
   )
 }
 
